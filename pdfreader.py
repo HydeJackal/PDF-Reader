bd19a474fca6f64643f0bb74885932f5249176bc
Jeffrey Shen <jcs528@cornell.edu>
2020-03-29 21:44:57 -0400

    Made App class for GUI

--- pdfreader.py
+++ pdfreader.py
@@ -2,9 +2,26 @@ from tkinter import *
 import reader
 import speaker
 
-window = Tk()
-label = Label(window, text="Welcome to the PDF reader.")
+class App:
 
-label.grid(row=0,column=0)
+  def __init__(self,master):
+    frame=Frame(master)
+    frame.pack()
+
+    self.label=Label(frame, text="Welcome to the PDF reader.")
+    self.label.pack()
+
+    self.button = Button(frame, text="Quit", command=frame.quit)
+    self.button.pack(side=LEFT)
+
+    self.hi_there = Button(frame, text="Hello", command=self.say_hi())
+    self.hi_there.pack(side=RIGHT)
+
+  def say_hi(self):
+      print("hi there, everyone!")
+
+window = Tk()
+app=App(window)
 
 window.mainloop()
+window.destory()
